07c00829afd55d0e134d10240358c8fd6e6c5c86
anclist <anclist@gmail.com>
2021-07-28 09:18:30 -0400

    Created cells and appended them to the grid element

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const grid = document.querySelector('.grid')
+const startButton = document.querySelector('#start')
+const score = document.querySelector('#score')
+let cells = []
 
+function createGrid() {
+
+    for ( let i = 0; i < 100 ; i++ ) {
+        let cell = document.createElement('div')
+        cell.classList.add('cell')
+        grid.appendChild(cell)
+        cells.push(cell)
+    }
+}
+
+createGrid()
